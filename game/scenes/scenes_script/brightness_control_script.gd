9c737bbc121b9bcf93fc0b3b5f3b7704f88b40e0
MJ <hopefulspicerabbits@gmail.com>
2025-09-07 14:12:07 -0500

    More Cleaning, Final One I Promise

--- game/scenes/scenes_script/brightness_control_script.gd
+++ game/scenes/scenes_script/brightness_control_script.gd
@@ -1,4 +1,5 @@
 extends Control
+
 @onready var slider = $Brightness_Slider
 @onready var label = $Brightness_Label
 @onready var value = Global_Values.player_information["brightness"]
@@ -22,3 +23,11 @@ func _on_brightness_slider_value_changed(current):
 
 func _on_close_settings_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://game/scenes/escp_menu_.tscn")
+
+
+func _on_fullscreen_button_pressed() -> void:
+	#error Embedded window only supports windowed mode
+	if true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
